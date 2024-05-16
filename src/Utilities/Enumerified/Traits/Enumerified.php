160b0f1281ed5346900103f997e68dcb2ef7ddba
GoodM4ven <goodm4ven@proton.me>
2024-05-16 06:44:48 +0300

    [v4.5.0] Fixed seeders installation and overhauled Enumerified trait

--- src/Utilities/Enumerified/Traits/Enumerified.php
+++ src/Utilities/Enumerified/Traits/Enumerified.php
@@ -4,64 +4,83 @@ declare(strict_types=1);
 
 namespace VPremiss\Crafty\Utilities\Enumerified\Traits;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use VPremiss\Crafty\Utilities\Enumerified\Support\Exceptions\EnumerifiedDuplicateEnumsException;
-use VPremiss\Crafty\Utilities\Enumerified\Support\Exceptions\EnumerifiedExcludedAllEnumsException;
-use VPremiss\Crafty\Utilities\Enumerified\Support\Exceptions\EnumerifiedInsufficientAmountException;
-use VPremiss\Crafty\Utilities\Enumerified\Support\Exceptions\EnumerifiedNoEnumCasesFoundException;
-use VPremiss\Crafty\Utilities\Enumerified\Support\Exceptions\EnumerifiedNotAnEnumException;
+use VPremiss\Crafty\Utilities\Enumerified\Support\Exceptions\EnumerifiedEnumException;
 
 trait Enumerified
 {
     public static function count(): int
     {
-        self::checkForCases();
+        self::validateCases();
 
         return count(self::cases());
     }
 
     public static function first(): self
     {
-        self::checkForCases();
+        self::validateCases();
 
         return self::cases()[0];
     }
 
-    public static function random(int $amount = 1, self|array $exceptFor = [], bool $asArray = false): self|array
-    {
+    public static function random(
+        int $amount = 1,
+        self|array $exceptFor = [],
+        bool $asArray = false,
+        bool $translated = false,
+    ): self|array {
         if ($amount < 1) {
-            throw new EnumerifiedInsufficientAmountException('The amount should be at least one.');
+            throw new EnumerifiedEnumException('The amount should be at least one.');
         }
 
-        self::checkForCases();
+        self::validateCases();
+        if ($translated) self::validateTranslations();
 
-        $exceptFor = [...$exceptFor];
-        $exceptFor = self::validatedEnums($exceptFor);
+        $exceptFor = self::filterEnums(Arr::wrap($exceptFor));
 
-        return collect(self::cases())
+        $result = collect(self::cases())
             ->reject(fn ($case) => in_array($case, $exceptFor))
             ->random($amount > 1 ? min($amount, self::count()) : ($asArray ? $amount : null));
+
+        if ($translated) {
+            $result = $result instanceof Collection
+                ? $result->map(fn ($item) => [$translated ? $item->translated() : $item->name => $item->value])->collapse()
+                : [$result->translated() => $result->value];
+        }
+
+        // ? Ensure an array
+        $result = $result instanceof Collection ? $result->toArray() : $result;
+        $result = $asArray ? Arr::wrap($result) : $result;
+
+        return $result;
     }
 
-    public static function names(self|array $exceptFor = []): array
+    public static function names(self|array $exceptFor = [], bool $translated = false): array
     {
-        self::checkForCases();
+        self::validateCases();
+        if ($translated) self::validateTranslations();
 
-        $exceptFor = [...$exceptFor];
-        $exceptFor = self::validatedEnums($exceptFor);
+        $exceptFor = self::filterEnums(Arr::wrap($exceptFor));
 
         return collect(self::cases())
             ->reject(fn ($case) => in_array($case, $exceptFor))
-            ->map(fn ($item) => $item->name)
+            ->map(fn ($item) => $translated ? $item->translated() : $item->name)
             ->toArray();
     }
 
-    public static function values(self|array $exceptFor = []): array
+    public static function values(self|array $exceptFor = [], bool $asString = false): array
     {
-        self::checkForCases();
+        self::validateCases();
 
-        $exceptFor = [...$exceptFor];
-        $exceptFor = self::validatedEnums($exceptFor);
+        $exceptFor = self::filterEnums(Arr::wrap($exceptFor));
+
+        if ($asString) {
+            return collect(self::cases())
+                ->reject(fn ($case) => in_array($case, $exceptFor))
+                ->pluck('value')
+                ->implode(',');
+        }
 
         return collect(self::cases())
             ->reject(fn ($case) => in_array($case, $exceptFor))
@@ -69,56 +88,52 @@ trait Enumerified
             ->toArray();
     }
 
-    public static function valuesAsString(self|array $exceptFor = []): string
+    public static function collection(self|array $exceptFor = [], bool $translated = true): Collection
     {
-        self::checkForCases();
+        self::validateCases();
+        if ($translated) self::validateTranslations();
 
-        $exceptFor = [...$exceptFor];
-        $exceptFor = self::validatedEnums($exceptFor);
+        $exceptFor = self::filterEnums(Arr::wrap($exceptFor));
 
         return collect(self::cases())
-            ->reject(fn ($case) => in_array($case, $exceptFor))
-            ->pluck('value')
-            ->implode(',');
-    }
-
-    public static function collection(self|array $exceptFor = [], $isTranslated = true): Collection
-    {
-        self::checkForCases();
-
-        $exceptFor = [...$exceptFor];
-        $exceptFor = self::validatedEnums($exceptFor);
-
-        return collect(self::cases())
-            ->map(fn ($item) => [$isTranslated ? $item->translated() : $item->name => $item->value])
+            ->map(fn ($item) => [$translated ? $item->translated() : $item->name => $item->value])
             ->collapse();
     }
 
-    private static function checkForCases(): void
+    private static function validateCases(): void
     {
         if (empty(self::cases())) {
-            throw new EnumerifiedNoEnumCasesFoundException('No enum cases were found!');
+            throw new EnumerifiedEnumException('No enum cases were found!');
         }
     }
 
-    private static function validatedEnums(array $enums): array
+    private static function filterEnums(array $enums): array
     {
         foreach ($enums as $enum) {
             if (!$enum instanceof self) {
-                throw new EnumerifiedNotAnEnumException('Not an enum instance was detected.');
+                throw new EnumerifiedEnumException("An instance which isn't an enum was detected.");
             }
         }
 
         $enumValues = array_map(fn ($enum) => $enum->value, $enums);
         if (count($enumValues) !== count(array_unique($enumValues))) {
-            throw new EnumerifiedDuplicateEnumsException('Duplicate enum cases were found.');
+            throw new EnumerifiedEnumException('Duplicate enum cases were found.');
         }
 
         $allCasesValues = array_map(fn ($case) => $case->value, self::cases());
         if (count($enumValues) === count($allCasesValues) && !array_diff($allCasesValues, $enumValues)) {
-            throw new EnumerifiedExcludedAllEnumsException('All possible enum cases were excluded!');
+            throw new EnumerifiedEnumException('All possible enum cases were excluded!');
         }
 
         return $enums;
+    }
+
+    protected static function validateTranslations(): void
+    {
+        if (!method_exists(self::class, 'translated')) {
+            throw new EnumerifiedEnumException(
+                "There isn't a 'translated' method on the enum that returns a matching case name string. So create one!"
+            );
+        }
     }
 }
